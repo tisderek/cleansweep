c1d99f228df1a55775cfeb5b9f5fa0cb6e9d9cd7
tisderek <derekgrsn@gmail.com>
2015-07-21 21:49:59 -0700

    stylesheets :e:

--- public/js/application.js
+++ public/js/application.js
@@ -2,6 +2,21 @@ $(document).ready(function() {
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
+  $('#park').hide();
+  $("#geolocate").on('click', function(){
+    $('#park').fadeIn('medium',function(){})
+  })
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('#park').on('click', function(){
+
+    var lat = myLayer._geojson.geometry.valueOf().coordinates[1];
+    var lng = myLayer._geojson.geometry.valueOf().coordinates[0];
+
+    $('#lat').attr('value', lat);
+    $('#lng').attr('value', lng);
+  })
+
 });
+
+/// Menu sidebar show ///
+
