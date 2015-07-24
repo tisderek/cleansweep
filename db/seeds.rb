924c81a9facca391a1d47c60fd18c59f000517ea
tisderek <derekgrsn@gmail.com>
2015-07-24 08:47:47 -0700

    add bees to seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,186 @@
 require 'rgeo/shapefile'
 
 User.create(
+    name: "Derek",
+    email: "the@admin.com",
+    phone_number: 4154168654,
+    password: 123123
+  )
+
+Contact.create(
+    name: "Leo",
+    phone_number: 4154168654,
+    user_id: 1
+  )
+
+User.create(
     name: "admin",
     email: "the@admin.com",
     phone_number: 0,
     password: 123123
   )
+User.create(
+    name: "Harvey",
+    email: "the@admin.com",
+    phone_number: 7406463285,
+    password: 123123
+  )
+User.create(
+    name: "Daniel Chen",
+    email: "the@admin.com",
+    phone_number: 4088588207,
+    password: 123123
+  )
+User.create(
+    name: "Marshall",
+    email: "the@admin.com",
+    phone_number: 6196722649,
+    password: 123123
+  )
+User.create(
+    name: "Jason",
+    email: "the@admin.com",
+    phone_number: 4086428138,
+    password: 123123
+  )
+User.create(
+    name: "Danny Shafer",
+    email: "the@admin.com",
+    phone_number: 5856439357,
+    password: 123123
+  )
+User.create(
+    name: "Andrew Patterson",
+    email: "the@admin.com",
+    phone_number: 2056168238,
+    password: 123123
+  )
+User.create(
+    name: "Ehtesham",
+    email: "the@admin.com",
+    phone_number: 4084874539,
+    password: 123123
+  )
+User.create(
+    name: "Sung",
+    email: "the@admin.com",
+    phone_number: 4159942058,
+    password: 123123
+  )
+User.create(
+    name: "Bikram",
+    email: "the@admin.com",
+    phone_number: 7314990321,
+    password: 123123
+  )
+User.create(
+    name: "Charlotte",
+    email: "the@admin.com",
+    phone_number: 5405208772,
+    password: 123123
+  )
+User.create(
+    name: "Lauryn",
+    email: "the@admin.com",
+    phone_number: 5038534277,
+    password: 123123
+  )
+User.create(
+    name: "John Hess",
+    email: "the@admin.com",
+    phone_number: 8052725632,
+    password: 123123
+  )
+User.create(
+    name: "Andrea",
+    email: "the@admin.com",
+    phone_number: 9787581400,
+    password: 123123
+  )
+User.create(
+    name: "Ma"Kevin" Yan",
+    email: "the@admin.com",
+    phone_number: 4089604418,
+    password: 123123
+  )
+User.create(
+    name: "Chris K",
+    email: "the@admin.com",
+    phone_number: 4156700774,
+    password: 123123
+  )
+User.create(
+    name: "Hooman",
+    email: "the@admin.com",
+    phone_number: 3108240774,
+    password: 123123
+  )
+User.create(
+    name: "Chris Scott",
+    email: "the@admin.com",
+    phone_number: 5109093590,
+    password: 123123
+  )
+User.create(
+    name: "Adam",
+    email: "the@admin.com",
+    phone_number: 5044180782,
+    password: 123123
+  )
+User.create(
+    name: "Tim",
+    email: "the@admin.com",
+    phone_number: 4159904786,
+    password: 123123
+  )
+User.create(
+    name: "Shahab",
+    email: "the@admin.com",
+    phone_number: 9259569660,
+    password: 123123
+  )
+User.create(
+    name: "Jacob Shafi",
+    email: "the@admin.com",
+    phone_number: 9259982763,
+    password: 123123
+  )
+User.create(
+    name: "Ryan Au",
+    email: "the@admin.com",
+    phone_number: 6265005826,
+    password: 123123
+  )
+User.create(
+    name: "Jacob Rogers",
+    email: "the@admin.com",
+    phone_number: 3107793957,
+    password: 123123
+  )
+User.create(
+    name: "Marisa",
+    email: "the@admin.com",
+    phone_number: 2142066892,
+    password: 123123
+  )
+User.create(
+    name: "Travis",
+    email: "the@admin.com",
+    phone_number: 8016525453,
+    password: 123123
+  )
+User.create(
+    name: "Aaron Jacon.",
+    email: "the@admin.com",
+    phone_number: 2488088695,
+    password: 123123
+  ) 
+User.create(
+    name: "Marshall",
+    email: "the@admin.com",
+    phone_number: 6196722649,
+    password: 123123
+)
 
 RGeo::Shapefile::Reader.open('db/routes/routes.shp') do |file|
   puts "File contains #{file.num_records} records."
